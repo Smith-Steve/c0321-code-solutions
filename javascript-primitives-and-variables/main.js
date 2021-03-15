47ec7db10b36b5173d61e5b761a03f34fbda5567
Steve-A-Dore <Steve.A.Dore@outlook.com>
2021-03-15 10:29:32 -0700

    javascript and primitives assignment

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+var fullName = 'Steve Smith';
+var isCool = true;
+var totalPets = 0;
+var unknown = null;
+var never;
+
+console.log('fullName: ', fullName);
+console.log('Is he cool?: ', isCool);
+console.log('Number of pets: ', totalPets);
+console.log('variable named unknown: ', unknown);
+console.log('variable never: ', never);
+
+console.log('fullName typeof: ', typeof fullName);
+console.log('Is he cool typeof: ', typeof isCool);
+console.log('Number of pets typeof: ', typeof totalPets);
+console.log('unknown type of: ', typeof unknown);
+console.log('never type of: ', typeof never);
