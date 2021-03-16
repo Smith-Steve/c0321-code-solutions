258386e820d57c8f3fa1d45fd331de96046ab1e9
Steve-A-Dore <Steve.A.Dore@outlook.com>
2021-03-16 15:30:12 -0700

    javascript-data-model

---
+++ javascript-data-model/index.js
@@ -0,0 +1,51 @@
+    Item: [{title: 'Javascript for impatient programmers',
+    author: 'Dr. Axel Raushmayer',
+    returndate: 10/7/2020,
+    deliveryDate: 8/8/2020,
+    priceInfo: 34.00,
+    productReview: false,
+    orderPlacement: 8/4/2020,
+    orderId: 114-399-1111111-2222-558
+    }]},
+    Item:
+          [{title: 'The Timeless Way of Building',
+            author: 'Chistopher Alexander',
+            returnDate: 8/19/2020,
+            deliveryDate: 7/20/2020,
+            priceInfo: 44.53,
+            productReview: false,
+            orderPlacement: 8/4/2020,
+            orderId: 114-399-1111111-2222-557
+          }]},
+   Item:
+            [
+            {title: 'Gamecube controller Adapter. Super Smash Bros.',
+            author: null,
+            returnDate: 8/5/2020,
+            deliveryDate: 7/20/2020,
+            priceInfo: 17.22,
+            productReview: false,
+            orderPlacement: 7/4/2020,
+            orderId: 114-399-1111111-2222-556
+           }
+          ]
+        },
+  [{Item:
+            [
+            {title: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+            author: null,
+            returnDate: 8/5/2020,
+            deliveryDate: 7/20/2020,
+            priceInfo: 94.95,
+            productReview: false,
+            orderPlacement: 7/3/2020,
+            orderId: 114-399-1111111-2222-555},
+            {title: 'The Art of SQL',
+            author: "Stephane Faroult",
+            returnDate: 8/4/2020,
+            deliveryDate: 7/5/2020,
+            priceInfo: 33.99,
+            productReview: false,
+            orderPlacement: 7/3/2020,
+            orderId: 114-399-1111111-2222-555},
+          ]
---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,18 @@
+var orderHistory =
+[
+  {
+    orderId: '114-399-1111111-2222-558',
+    orderPlacement: '8/4/2020',
+    priceInfo: 34.50,
+    deliveryDate: '8/8/2020',
+    orderItems: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Christopher Alexander',
+        returnDate: '8/19/2020',
+        price: 34.40,
+        productReview: false
+      }
+    ]
+  }
+];
