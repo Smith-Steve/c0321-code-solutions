61c3348afdca4b827d8e0ea00ed16ba46ce3d24b
Steve-A-Dore <Steve.A.Dore@outlook.com>
2021-03-16 15:58:13 -0700

    I think I finally got it.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -7,10 +7,60 @@ var orderHistory =
     deliveryDate: '8/8/2020',
     orderItems: [
       {
-        title: 'The Timeless Way of Building',
+        title: 'Javascript for the impatient programmers',
         author: 'Christopher Alexander',
         returnDate: '8/19/2020',
         price: 34.40,
+        productReview: false
+      }
+    ]
+  },
+  {
+    orderId: '114-399-1111111-2222-577',
+    orderPlacement: '8/4/2020',
+    priceInfo: 44.53,
+    deliveryDate: '8/9/2020',
+    orderItems: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Dr. Axel Raushmayer',
+        returnDate: '8/19/2020',
+        price: 44.30,
+        productReview: false
+      }
+    ]
+  },
+  {
+    orderId: '114-399-1111111-2222-658',
+    orderPlacement: '8/6/2020',
+    priceInfo: 17.22,
+    deliveryDate: '8/10/2020',
+    orderItems: [
+      {
+        title: 'Camecube Controller Adapter. Super Smash Bros',
+        returnDate: '8/19/2020',
+        price: 17.22,
+        productReview: false
+      }
+    ]
+  },
+  {
+    orderId: '114-399-1111111-2222-778',
+    orderPlacement: '8/4/2020',
+    priceInfo: 138.93,
+    deliveryDate: '8/8/2020',
+    orderItems: [
+      {
+        title: 'Gamecube Controller - Super Smash Bros. Edition',
+        returnDate: '8/19/2020',
+        price: 94.95,
+        productReview: false
+      },
+      {
+        title: 'The Art of SQL',
+        author: 'Stephane Faroult',
+        returnDate: '8/19/2020',
+        price: 33.99,
         productReview: false
       }
     ]
