ee7d0b61e421166f153bbf3dbf06d6a6400322f1
Steve-A-Dore <Steve.A.Dore@outlook.com>
2021-03-26 16:48:03 -0700

    object-manipulation-2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,15 @@
 /* exported omit */
+function omit(source, keys) {
+  var newObject = {};
+  var increment = 0;
+  for (var key in source) {
+    if (keys[increment] !== source[key]) {
+      increment++;
+      newObject[key] = source[key];
+
+    } else {
+      increment++;
+    }
+  }
+  return newObject;
+}
