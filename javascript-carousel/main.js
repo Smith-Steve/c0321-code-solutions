c8b2e7e110704abca4585872c1d2c7479ad0be35
Steve-A-Dore <Steve.A.Dore@outlook.com>
2021-04-08 15:41:57 -0700

    hello

---
+++ javascript-carousel/main.js
@@ -0,0 +1,25 @@
+var $iconSelectedImage = document.querySelector('.fas.fa-star');
+var $iconOtherImage = document.querySelector('.far.fa-star');
+var $icon = document.querySelectorAll('.fa-star');
+
+
+function handleClickIcon(event) {
+  var arrayOfIcons = $parentListener[0].children;
+  console.log(event.target);
+
+  for (let i = 0; i < arrayOfIcons.length; i++) {
+    var icon = arrayOfIcons[i];
+    console.log(arrayOfIcons[i], 'the index printing is ', i)
+    if(icon === event.target) {
+      event.target.setAttribute('class', 'fas fa-star');
+      console.log(event.target, 'i am the event target')
+    } else if (icon === 'fas fa-star') {
+      icon.setAttribute('class', 'far fa-star')
+    } else {
+      icon.setAttribute('class', 'far fa-star')
+    }
+  }
+}
+
+$iconSelectedImage.addEventListener('click', handleClickIcon);
+$iconOtherImage.addEventListener('click', handleClickIcon);
