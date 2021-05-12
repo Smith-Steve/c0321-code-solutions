a4bcf94b46243b8bb5986208dd3ca2448fb79969
Smith-Steve <Steve.A.Dore@outlook.com>
2021-05-11 17:59:18 -0700

    es6-classes

---
+++ es6-classes/stopwatch.js
@@ -0,0 +1,37 @@
+/* exported Stopwatch */
+class StopWatch {
+
+  constructor(startTimeInSeconds) {
+    this.elapsedSeconds = startTimeInSeconds;
+  }
+
+  tick() {
+    return this.elapsedSeconds++;
+  }
+
+  getTime() {
+    const secondsPerHour = 3600;
+    const secondsPerMinute = 60;
+    let seconds = this.elapsedSeconds;
+    let hours = Math.floor(seconds / secondsPerHour);
+    seconds -= (secondsPerHour * hours);
+    let minutes = Math.floor(seconds / secondsPerMinute);
+    seconds -= (secondsPerMinute * minutes);
+    hours = hours.toString().padStart(2, '0');
+    minutes = minutes.toString().padStart(2, '0');
+    seconds = seconds.toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  reset() {
+    this.elapsedSeconds = 0;
+  }
+}
+
+const watch = new StopWatch(30);
+// eslint-disable-next-line
+console.log(watch.tick());
+// eslint-disable-next-line
+console.log(watch.getTime());
+// eslint-disable-next-line
+console.log(watch.reset());
